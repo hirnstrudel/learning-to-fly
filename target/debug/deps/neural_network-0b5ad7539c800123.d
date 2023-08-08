0a3ac1c458b7e673e1200c30ade75e42f251d717
hirnstrudel <benjamin.billian@gmail.com>
2023-08-08 16:57:46 +0200

    first commit

---
+++ target/debug/deps/neural_network-0b5ad7539c800123.d
@@ -0,0 +1,8 @@
+/home/benjamin/rust/learning-to-fly/target/debug/deps/neural_network-0b5ad7539c800123.rmeta: libs/neural-network/src/lib.rs Cargo.toml
+
+/home/benjamin/rust/learning-to-fly/target/debug/deps/neural_network-0b5ad7539c800123.d: libs/neural-network/src/lib.rs Cargo.toml
+
+libs/neural-network/src/lib.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
---
+++ target/debug/deps/neural_network-2b1468db9f06e811.d
@@ -0,0 +1,8 @@
+/home/benjamin/rust/learning-to-fly/target/debug/deps/neural_network-2b1468db9f06e811.rmeta: libs/neural-network/src/lib.rs Cargo.toml
+
+/home/benjamin/rust/learning-to-fly/target/debug/deps/neural_network-2b1468db9f06e811.d: libs/neural-network/src/lib.rs Cargo.toml
+
+libs/neural-network/src/lib.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
