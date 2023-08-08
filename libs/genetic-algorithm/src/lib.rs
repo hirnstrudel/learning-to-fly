729182ce1db3b86b5a9c717d98b8c8fd6450549b
hirnstrudel <benjamin.billian@gmail.com>
2023-08-08 23:34:03 +0200

    genetic algorithm stub

---
+++ libs/genetic-algorithm/src/lib.rs
@@ -0,0 +1,26 @@
+pub struct GeneticAlgorithm;
+
+impl GeneticAlgorithm {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn evolve<I>(&self, population: &[I]) -> Vec<I> {
+        assert!(!population.is_empty());
+
+        (0..population.len())
+            .map(|_| {
+                // TODO selection
+                // TODO crossover
+                // TODO mutation
+                todo!()
+            })
+            .collect()
+    }
+}
+
+impl Default for GeneticAlgorithm {
+    fn default() -> Self {
+        Self::new()
+    }
+}
