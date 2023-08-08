e31fa8ea6f220080c666605474fb9ed3887c29a3
hirnstrudel <benjamin.billian@gmail.com>
2023-08-08 22:05:48 +0200

    cleanup

--- target/debug/deps/neural_network-0b5ad7539c800123.d
+++
@@ -1,8 +0,0 @@
-/home/benjamin/rust/learning-to-fly/target/debug/deps/neural_network-0b5ad7539c800123.rmeta: libs/neural-network/src/lib.rs Cargo.toml
-
-/home/benjamin/rust/learning-to-fly/target/debug/deps/neural_network-0b5ad7539c800123.d: libs/neural-network/src/lib.rs Cargo.toml
-
-libs/neural-network/src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
--- target/debug/deps/neural_network-2b1468db9f06e811.d
+++
@@ -1,8 +0,0 @@
-/home/benjamin/rust/learning-to-fly/target/debug/deps/neural_network-2b1468db9f06e811.rmeta: libs/neural-network/src/lib.rs Cargo.toml
-
-/home/benjamin/rust/learning-to-fly/target/debug/deps/neural_network-2b1468db9f06e811.d: libs/neural-network/src/lib.rs Cargo.toml
-
-libs/neural-network/src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
